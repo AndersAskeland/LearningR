7d731bae47bc0356e19c6e5cd0b58506ecc02d42
Anders Askeland <anders.askeland@gmail.com>
2023-01-16 16:12:32 +0100

    Initial commit

---
+++ R/learning.R
@@ -0,0 +1,19 @@
+library(tidyverse)
+
+# Making code more readable section (Styler) ------------------------------------------------------------
+
+# Object names
+DayOne
+T <- FALSE
+c <- 9
+
+# Spacing
+x[, 1]
+x[, 1]
+mean(x, na.rm = TRUE)
+mean(x, na.rm = TRUE)
+height <- feet * 12 + inches
+df$ z
+x <- 1:10
+
+
